2de6638a2f14dd399a63df05268746e57758be4e
嘉登糖 <77535733+JesterRomut@users.noreply.github.com>
2025-05-09 09:03:45 +0800

    Create +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { page } from '$app/state';
+	import { locales, localizeHref } from '$lib/paraglide/runtime';
+
+let {children} = $props()
+</script>
+
+{@render children()}
+
+<div style="display:none">
+ {#each locales as locale}
+ <a href={localizeHref(page.url.pathname, { locale })}>{locale}</a>
+ {/each}
++</div>
